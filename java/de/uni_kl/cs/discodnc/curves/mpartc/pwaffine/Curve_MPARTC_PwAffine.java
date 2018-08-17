867ffcff184fa4b5e4f064d763caeb76f278a377
sbondorf <steffen@s-bondorf.de>
2018-08-17 17:34:24 -0400

    Brought back master's unboundedIinfinite arrivals
    ArrivalCurve createUnboundedArrivals() is now ArrivalCurve
    createInfiniteArrivals().

--- java/de/uni_kl/cs/discodnc/curves/mpartc/pwaffine/Curve_MPARTC_PwAffine.java
+++ java/de/uni_kl/cs/discodnc/curves/mpartc/pwaffine/Curve_MPARTC_PwAffine.java
@@ -830,6 +830,12 @@ public class Curve_MPARTC_PwAffine implements Curve_PwAffine {
 		return new ArrivalCurve_MPARTC_PwAffine(); // ArrivalCurveRTC constructor's default behavior
 	}
 
+	public ArrivalCurve_MPARTC_PwAffine createInfiniteArrivals() {
+		ArrivalCurve_MPARTC_PwAffine ac_rtc = new ArrivalCurve_MPARTC_PwAffine(); 
+		makeDelayedInfiniteBurst(ac_rtc, 0.0);
+		return ac_rtc;
+	}
+
 	public ArrivalCurve_MPARTC_PwAffine createPeakArrivalRate(double rate) {
 		ArrivalCurve_MPARTC_PwAffine ac_rtc = new ArrivalCurve_MPARTC_PwAffine();
 		makePeakRate(ac_rtc, rate);
