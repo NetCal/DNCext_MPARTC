6186bcd1038f1e0792342fbd06436759b26a88d0
sbondorf <steffen@s-bondorf.de>
2018-12-01 11:15:00 +0100

    Adaptation required to work after merging DiscoDNC PR #52

--- java/de/uni_kl/cs/discodnc/algebra/mpa_rtc_pwaffine/MinPlus_MPARTC.java
+++ java/de/uni_kl/cs/discodnc/algebra/mpa_rtc_pwaffine/MinPlus_MPARTC.java
@@ -65,7 +65,7 @@ public enum MinPlus_MPARTC implements MinPlus {
 				((Curve_MPARTC_PwAffine) service_curve_1).getRtc_curve(),
 				((Curve_MPARTC_PwAffine) service_curve_2).getRtc_curve());
 
-		return Calculator.getInstance().getCurve().createServiceCurve(result.toString());
+		return Calculator.getInstance().getCurveFactory().createServiceCurve(result.toString());
 	}
 
 	// Java won't let us call this method "convolve" because it does not care about
@@ -94,7 +94,7 @@ public enum MinPlus_MPARTC implements MinPlus {
 				Curve_MPARTC_PwAffine s11 = (Curve_MPARTC_PwAffine) beta_1;
 				Curve_MPARTC_PwAffine s12 = (Curve_MPARTC_PwAffine) beta_2;
 
-				results.add(Calculator.getInstance().getCurve()
+				results.add(Calculator.getInstance().getCurveFactory()
 						.createServiceCurve(CurveMath.minPlusConv(s11.getRtc_curve(), s12.getRtc_curve()).toString()));
 			}
 		}
@@ -108,7 +108,7 @@ public enum MinPlus_MPARTC implements MinPlus {
 				((Curve_MPARTC_PwAffine) arrival_curve_1).getRtc_curve(),
 				((Curve_MPARTC_PwAffine) arrival_curve_2).getRtc_curve());
 
-		return Calculator.getInstance().getCurve().createArrivalCurve(result.toString());
+		return Calculator.getInstance().getCurveFactory().createArrivalCurve(result.toString());
 	}
 
 	public ArrivalCurve convolve(Set<ArrivalCurve> arrival_curves) throws Exception {
@@ -116,7 +116,7 @@ public enum MinPlus_MPARTC implements MinPlus {
 
 		// TODO Double check
 		if (arrival_curves == null || arrival_curves.isEmpty()) {
-			return Calculator.getInstance().getCurve().createZeroArrivals();
+			return Calculator.getInstance().getCurveFactory().createZeroArrivals();
 		}
 		if (arrival_curves.size() == 1) {
 			return arrival_curves.iterator().next().copy();
@@ -127,14 +127,14 @@ public enum MinPlus_MPARTC implements MinPlus {
 		ch.ethz.rtc.kernel.Curve result = new ch.ethz.rtc.kernel.Curve(sl);
 		ch.ethz.rtc.kernel.Curve ac2 = null;
 		for (ArrivalCurve arrival_curve_2 : arrival_curves) {
-			ArrivalCurve result_curves = Calculator.getInstance().getCurve().createArrivalCurve(arrival_curve_2.toString());
+			ArrivalCurve result_curves = Calculator.getInstance().getCurveFactory().createArrivalCurve(arrival_curve_2.toString());
 			Curve_MPARTC_PwAffine c = (Curve_MPARTC_PwAffine) result_curves;
 			ac2 = c.getRtc_curve();
 
 			result = CurveMath.minPlusConv(result, ac2);
 		}
 
-		return Calculator.getInstance().getCurve().createArrivalCurve(ac2.toString());
+		return Calculator.getInstance().getCurveFactory().createArrivalCurve(ac2.toString());
 	}
 
 	// Maximum Service Curves
@@ -145,7 +145,7 @@ public enum MinPlus_MPARTC implements MinPlus {
 				((Curve_MPARTC_PwAffine) max_service_curve_1).getRtc_curve(),
 				((Curve_MPARTC_PwAffine) max_service_curve_2).getRtc_curve());
 
-		return Calculator.getInstance().getCurve().createMaxServiceCurve(result.toString());
+		return Calculator.getInstance().getCurveFactory().createMaxServiceCurve(result.toString());
 	}
 
 	// Arrival Curves and Max Service Curves
@@ -158,7 +158,7 @@ public enum MinPlus_MPARTC implements MinPlus {
 		Curve_MPARTC_PwAffine msc_mpa_rtc = (Curve_MPARTC_PwAffine) maximum_service_curve;
 		for (ArrivalCurve alpha_tmp : arrival_curves) {
 			// Do not mind the semantics "Arrival Curve"
-			results.add(Calculator.getInstance().getCurve()
+			results.add(Calculator.getInstance().getCurveFactory()
 					.createArrivalCurve(CurveMath
 							.minPlusConv(((Curve_MPARTC_PwAffine) alpha_tmp).getRtc_curve(), msc_mpa_rtc.getRtc_curve())
 							.toString()));
@@ -174,7 +174,7 @@ public enum MinPlus_MPARTC implements MinPlus {
 
 		Curve_MPARTC_PwAffine egamma_mpa_rtc = (Curve_MPARTC_PwAffine) extra_gamma_curve;
 		for (ArrivalCurve alpha_tmp : arrival_curves) {
-			results.add(Calculator.getInstance().getCurve().createArrivalCurve(CurveMath
+			results.add(Calculator.getInstance().getCurveFactory().createArrivalCurve(CurveMath
 					.minPlusConv(((Curve_MPARTC_PwAffine) alpha_tmp).getRtc_curve(), egamma_mpa_rtc.getRtc_curve())
 					.toString()));
 		}
@@ -196,7 +196,7 @@ public enum MinPlus_MPARTC implements MinPlus {
 
 		Curve_MPARTC_PwAffine beta_mpa_rtc = (Curve_MPARTC_PwAffine) service_curve;
 		for (ArrivalCurve alpha_tmp : arrival_curves) {
-			results.add(Calculator.getInstance().getCurve().createArrivalCurve(CurveMath
+			results.add(Calculator.getInstance().getCurveFactory().createArrivalCurve(CurveMath
 					.minPlusDeconv(((Curve_MPARTC_PwAffine) alpha_tmp).getRtc_curve(), beta_mpa_rtc.getRtc_curve())
 					.toString()));
 		}
@@ -216,7 +216,7 @@ public enum MinPlus_MPARTC implements MinPlus {
 
 		for (ServiceCurve beta_tmp : service_curves) {
 			for (ArrivalCurve alpha_tmp : arrival_curves) {
-				results.add(Calculator.getInstance().getCurve()
+				results.add(Calculator.getInstance().getCurveFactory()
 						.createArrivalCurve(CurveMath.minPlusDeconv(((Curve_MPARTC_PwAffine) alpha_tmp).getRtc_curve(),
 								((Curve_MPARTC_PwAffine) beta_tmp).getRtc_curve()).toString()));
 			}
@@ -236,7 +236,7 @@ public enum MinPlus_MPARTC implements MinPlus {
 				((Curve_MPARTC_PwAffine) arrival_curve).getRtc_curve(),
 				((Curve_MPARTC_PwAffine) service_curve).getRtc_curve());
 
-		return Calculator.getInstance().getCurve().createArrivalCurve(result.toString());
+		return Calculator.getInstance().getCurveFactory().createArrivalCurve(result.toString());
 	}
 
 	public Set<ArrivalCurve> deconvolve_almostConcCs_SCs(Set<Curve> curves, Set<ServiceCurve> service_curves)
@@ -248,7 +248,7 @@ public enum MinPlus_MPARTC implements MinPlus {
 		for (ServiceCurve beta_tmp : service_curves) {
 			for (Curve c_tmp : curves) {
 				// Do not mind the semantics "Arrival Curve"
-				results.add(Calculator.getInstance().getCurve()
+				results.add(Calculator.getInstance().getCurveFactory()
 						.createArrivalCurve(CurveMath.minPlusDeconv(((Curve_MPARTC_PwAffine) c_tmp).getRtc_curve(),
 								((Curve_MPARTC_PwAffine) beta_tmp).getRtc_curve()).toString()));
 			}
